e6a87e055d6d3160b127c28fa6b81ea5da6d33d4
Nathan Walker <walkerrunpdx@gmail.com>
2018-01-16 22:02:28 -0800

    rename

--- apps/mobile/app/modules/core/services/window-mobile.service.ts
+++ apps/mobile/app/modules/core/services/window-mobile.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 
 // libs
 import { alert as tnsAlert, confirm as tnsConfirm } from 'tns-core-modules/ui/dialogs';
-import { WindowPlatformService } from '@mycompany/core';
+import { PlatformWindowService } from '@mycompany/core';
 
 @Injectable()
-export class WindowMobileService extends WindowPlatformService {
+export class WindowMobileService extends PlatformWindowService {
 
   public alert(msg: any) {
     return tnsAlert(msg);
